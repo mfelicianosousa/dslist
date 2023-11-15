fb5aa6602a3d2501ed57336eb679ffc0af89d495
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2023-11-15 15:07:14 -0400

    Consultas SQL, Projections

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
 	private long id;
 	private String title;
+	private Integer year;
 	private String imgUrl;
 	private String shortDescription;
 	
@@ -17,8 +19,18 @@ public class GameMinDTO {
 
 		id = entity.getId();
 		title = entity.getTitle();
+		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public long getId() {
@@ -27,6 +39,10 @@ public class GameMinDTO {
 
 	public String getTitle() {
 		return title;
+	}
+	
+	public Integer getYear() {
+		return year;
 	}
 
 	public String getImgUrl() {
--- src/main/java/com/devsuperior/dslist/service/GameService.java
+++ src/main/java/com/devsuperior/dslist/service/GameService.java
@@ -10,6 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 
@@ -39,5 +40,15 @@ public class GameService {
 		
 		return gameDTO ;
 	}
+	
+	@Transactional(readOnly=true)
+	public List<GameMinDTO> findByList(Long listId){
+		
+		List<GameMinProjection> result = gameRepository.searchByList( listId );
+		return  result.stream().map(x -> new GameMinDTO(x)).toList();
+
+	}
+	
+	
 
 }
